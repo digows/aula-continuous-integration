e4e74f7678006b2a83185c72b6ed5375bab0b7dc
Diogo A. <diogo.augustoo@gmail.com>
2015-10-26 21:19:26 -0200

    Dois testes de frear

--- src/test/java/br/com/aula/ci/tests/CarroTest.java
+++ src/test/java/br/com/aula/ci/tests/CarroTest.java
@@ -17,4 +17,34 @@ public class CarroTest {
 		Assert.assertEquals( 0, carro.getGirosMotor(), 0 );
 		Assert.assertEquals( Cambio.NEUTRO, carro.getMarcha());
 	}
+	
+	@Test
+	public void validaCarroEstarMovimento(){
+		final Carro carro = new Carro();
+		carro.ligar();
+		Assert.assertTrue(carro.isLigado());
+		
+		Assert.assertEquals(Cambio.NEUTRO, carro.getMarcha());
+		
+		carro.trocarMarcha(Cambio.PRIMEIRA_MARCA);
+		carro.acelerar(10);
+		
+		Assert.assertTrue(carro.getVelocidade() > 0);
+	}
+	
+	@Test
+	public void validaCarroFrearEmDezPorcentoENaoVaiParar(){
+		final Carro carro = new Carro();
+		carro.ligar();
+		Assert.assertTrue(carro.isLigado());
+		Assert.assertEquals(Cambio.NEUTRO, carro.getMarcha());
+		
+		carro.trocarMarcha(Cambio.PRIMEIRA_MARCA);
+		carro.acelerar(10);
+		
+		carro.freiar(1);
+		Assert.assertTrue(carro.getVelocidade() == 90);
+	}
+	
+	
 }
